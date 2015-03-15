318d5478e0ece33bbeb8743bcc57eedb9c0a265c
Sherry Zhou <5643f56c@opayq.com>
2015-03-15 10:12:24 -0700

    Fix spacing

--- db/migrate/20150313234647_create_items.rb
+++ db/migrate/20150313234647_create_items.rb
@@ -1,10 +1,10 @@
 class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
-      t.integer :parent_id
-      t.string  :title
-      t.text    :content
-      t.integer :value
+      t.integer  :parent_id
+      t.string   :title
+      t.text     :content
+      t.integer  :value
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20150313234647) do
-
-  create_table "items", force: :cascade do |t|
-    t.integer "parent_id"
-    t.string  "title"
-    t.text    "content"
-    t.integer "value"
+  create_table 'items', force: :cascade do |t|
+    t.integer  'parent_id'
+    t.string   'title'
+    t.text     'content'
+    t.integer  'value'
   end
-
 end
